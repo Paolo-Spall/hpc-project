f0f7a70425344d207c137c7140ea4d46cd9512fc
Flavia Nironi <flavianironi@MacBookPro.home-life.hub>
2025-06-24 12:03:14 +0200

    final_fortran

--- fortran/ProdVectMat-Gather.f90
+++ fortran/ProdVectMat-Gather.f90
@@ -2,13 +2,15 @@ program vect_mat_mul
     USE MPI
     implicit none
     
+    integer, parameter :: K2 = selected_real_kind(15, 30)
     integer :: n,i,j,k,st, remainder, disp
     integer :: size, ierr, ecode, rank , status(MPI_STATUS_SIZE)
     real :: cpu1, cpu2, deltat
-    real, allocatable, dimension(:,:) :: M
-    real, allocatable, dimension(:) :: V, C, buffer
+    real(KIND=K2), allocatable, dimension(:,:) :: M
+    real(KIND=K2), allocatable, dimension(:) :: V, C, buffer
     integer, allocatable, dimension(:) :: DISPLS, RECVCOUNTS
     integer :: Ncolumns
+    character(len=100) :: times_filename
 
     call MPI_INIT(ierr)
     call MPI_COMM_SIZE(MPI_COMM_WORLD,size,ierr)
@@ -20,6 +22,7 @@ program vect_mat_mul
 
     OPEN(11,FILE="input_n.txt",STATUS="OLD",IOSTAT=st)
     READ(11,*,IOSTAT=st) n
+    CLOSE(11, STATUS='KEEP')
 
     print*, 'Dimension of the matrix:', n
 
@@ -74,20 +77,39 @@ program vect_mat_mul
         end do
     end do
     
-    do i=1,n
-        print*, M(i,:)
-    end do
+    ! do i=1,n
+    !     print*, M(i,:)
+    ! end do
 
     ! initialize resulting vector
     allocate(C(n))
 
-    C = MATMUL(M,V)
+    C = MATMUL(V, M)
+
+    !communication part
+    allocate(buffer(n))
+    call MPI_GATHERV(C, Ncolumns, MPI_DOUBLE_PRECISION, buffer, RECVCOUNTS, DISPLS, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
+    if (rank == 0) then
+        ! The root process gathers the results
+        ! print '(A,10(E15.7E3,1X))', 'Middle ten gathered results:', buffer(496:505)
+        ! print*, 'Last five elements of the gathered vector:', buffer(n-4:n)
+        OPEN(12,FILE="ProdVectMat-Gather_f90.output",STATUS="REPLACE")
+        do i=1, 100
+            WRITE(12,*) buffer(i)
+        end do
+        CLOSE(12, STATUS='KEEP')
+    end if
 
     CALL CPU_TIME(cpu2)
 
     deltat = cpu2 - cpu1
-
-    print*, 'Time taken for initialization:', deltat
+    if (rank == 0) then
+        WRITE(times_filename, '(A,I0,A)') 'times_', n, '_ProdVectMat-Gather_f90.txt'
+        OPEN(13,FILE=times_filename,status='unknown', position='append')
+        WRITE(13, '(I2, X, F10.6)') size, deltat
+        CLOSE(13, STATUS='KEEP')
+        !print*, 'Time taken for initialization:', deltat
+    end if
 
     CALL MPI_FINALIZE(ierr)
 end program vect_mat_mul
