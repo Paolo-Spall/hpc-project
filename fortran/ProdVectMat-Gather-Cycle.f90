65f5690dca5eec50ad8f035dfa9d64a28f4b7da9
Paolo Spallanzani <spallanzanipaolo90@gmail.com>
2025-06-27 14:53:29 +0200

    fortran commented

--- fortran/ProdVectMat-Gather-Cycle.f90
+++ fortran/ProdVectMat-Gather-Cycle.f90
@@ -1,40 +1,39 @@
 program vect_mat_mul
     USE MPI
     implicit none
-    
+    ! defining double precision tipe
     integer, parameter :: K2 = selected_real_kind(15, 30)
-    integer :: n,i,j,k,st, remainder, disp
-    integer :: size, ierr, ecode, rank , status(MPI_STATUS_SIZE)
+    ! declaring indexes and integer aux variables
+    integer :: n,i,j,k,st, Ncolumns, remainder, disp 
+    integer :: size, ierr, ecode, rank , status(MPI_STATUS_SIZE) ! decl. mpi variables
     real :: cpu1, cpu2, deltat
+    ! declaring matrix and vector variables
     real(KIND=K2), allocatable, dimension(:,:) :: M
     real(KIND=K2), allocatable, dimension(:) :: V, C, buffer
     integer, allocatable, dimension(:) :: DISPLS, RECVCOUNTS
-    integer :: Ncolumns
+    ! filename strings
     character(len=100) :: times_filename
 
-    call MPI_INIT(ierr)
-    call MPI_COMM_SIZE(MPI_COMM_WORLD,size,ierr)
-    call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierr)
+    ! initializing parallel part of the code
+    call MPI_INIT(ierr) ! global communicator
+    call MPI_COMM_SIZE(MPI_COMM_WORLD,size,ierr) !extracting task id
+    call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierr) !extracting number of processes
     
+    !starting time measuring
     CALL CPU_TIME(cpu1)
 
-    print*, 'MPI initialized. Rank:', rank, 'of', size
-
+    ! Read the size of the matrix from a file
     OPEN(11,FILE="input_n.txt",STATUS="OLD",IOSTAT=st)
     READ(11,*,IOSTAT=st) n
     CLOSE(11, STATUS='KEEP')
 
-    print*, 'Dimension of the matrix:', n
-
     !computing number of columns that each process will handle
-    print*, 'Number of processes:', size
     Ncolumns = n / size
     remainder = mod(n, size)
-    print*, 'Number of columns per process:', Ncolumns
 
     ! Setting DISPLS and RECVCOUNTS for MPI_GATHER
-    allocate(DISPLS(size))
-    allocate(RECVCOUNTS(size))
+    allocate(DISPLS(size)) !width of resulting vector for each process
+    allocate(RECVCOUNTS(size)) ! index of the first element of the local vector in the global one
 
     disp = 0
     do i=0, size-1
@@ -47,44 +46,33 @@ program vect_mat_mul
         disp = disp + RECVCOUNTS(i+1)
     end do
 
-    print*, 'DISPLS:', DISPLS
-    print*, 'RECVCOUNTS:', RECVCOUNTS
-    !if 
-
     ! If the number of columns is not divisible by size increase by one
-    ! the first elements
+    ! the first x elements, with x being the reminder of n/size
    
     if (rank < remainder) then
         Ncolumns = Ncolumns + 1
     end if
 
-    print*, 'Adjusted number of columns for process', rank, ':', Ncolumns
-
-    ! Initialize Vector
+    ! Initialize Vector as [ 1,2,...,n ]
     allocate(V(n))   
     
     do i=1,n
         V(i) = i
     end do
 
-    ! Initialize Matrix
+    ! Initialize Matrix with n rows, each one like [ 1,2,...,n ]
     allocate(M(n,Ncolumns))
 
     do i=1,n
         do j = 1, Ncolumns
-        !do j=DISPLS(rank)+1, DISPLS(rank)+1 + RECVCOUNTS(rank)
             M(i, j) = DISPLS(rank+1) + j
         end do
     end do
-    
-    ! do i=1,n
-    !     print*, M(i,:)
-    ! end do
 
     ! initialize resulting vector
     allocate(C(Ncolumns))
 
-    !C = MATMUL(V, M)
+    !Computing the vector-matrix product
     do j =1, Ncolumns
         C(j) = 0.0
         do k=1, n
@@ -92,22 +80,26 @@ program vect_mat_mul
         end do
     end do
 
-    !communication part
-    allocate(buffer(n))
+    ! COMMUNICATION SECTION
+    allocate(buffer(n)) ! initiate the buffer
+    !gathering all the result vectors C into buffer
     call MPI_GATHERV(C, Ncolumns, MPI_DOUBLE_PRECISION, buffer, RECVCOUNTS, DISPLS, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
+    
+    ! SAVING THE DATA
     if (rank == 0) then
         ! The root process gathers the results
-        ! print '(A,10(E15.7E3,1X))', 'Middle ten gathered results:', buffer(496:505)
-        ! print*, 'Last five elements of the gathered vector:', buffer(n-4:n)
         OPEN(12,FILE="ProdVectMat-Gather-Cycle_f90.output",STATUS="REPLACE")
+        ! writing the firsts 100 elements in the output file
         do i=1, min(n,100)
             WRITE(12,*) buffer(i)
         end do
         CLOSE(12, STATUS='KEEP')
     end if
 
+    ! stop time measuring
     CALL CPU_TIME(cpu2)
 
+    ! appending the time measurement in the correspondant time file
     deltat = cpu2 - cpu1
     if (rank == 0) then
         WRITE(times_filename, '(A,I0,A)') 'times_', n, '_ProdVectMat-Gather-Cycle_f90.txt'
@@ -117,6 +109,7 @@ program vect_mat_mul
         !print*, 'Time taken for initialization:', deltat
     end if
 
+    ! finalizing the parallel code 
     CALL MPI_FINALIZE(ierr)
 end program vect_mat_mul
 
--- fortran/ProdVectMat-Receive-Cycle.f90
+++ fortran/ProdVectMat-Receive-Cycle.f90
@@ -2,30 +2,32 @@ program vect_mat_mul
     USE MPI
     implicit none
     
+    ! defining double precision tipe
     integer, parameter :: K2 = selected_real_kind(15, 30)
-    integer :: n,i,j,k,st, remainder, disp, n_print, n_elements
-    ! MPI variables
+    ! declaring indexes and integer aux variables
+    integer :: n,i,j,k, Ncolumns, st, remainder, disp, n_print, n_elements
+    ! decl MPI variables
     integer :: size, ierr, ecode, rank , status(MPI_STATUS_SIZE)
     real :: cpu1, cpu2, deltat
+    ! declaring matrix and vector variables
     real(KIND=K2), allocatable, dimension(:,:) :: M
     real(KIND=K2), allocatable, dimension(:) :: V, C, buffer
     integer, allocatable, dimension(:) :: DISPLS, RECVCOUNTS
-    integer :: Ncolumns
+    ! filename strings
     character(len=100) :: times_filename
 
-    call MPI_INIT(ierr)
-    call MPI_COMM_SIZE(MPI_COMM_WORLD,size,ierr)
-    call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierr)
+    ! initializing parallel part of the code
+    call MPI_INIT(ierr) ! global communicator
+    call MPI_COMM_SIZE(MPI_COMM_WORLD,size,ierr) !extracting task id
+    call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierr)!extracting number of processes
     
+    !starting time measuring
     CALL CPU_TIME(cpu1)
 
-    print*, 'MPI initialized. Rank:', rank, 'of', size
-
+    ! Read the size of the matrix from a file
     OPEN(11,FILE="input_n.txt",STATUS="OLD",IOSTAT=st)
     READ(11,*,IOSTAT=st) n
     CLOSE(11, STATUS='KEEP')
-
-    print*, 'Dimension of the matrix:', n
 
     !computing number of columns that each process will handle
     print*, 'Number of processes:', size
@@ -34,8 +36,8 @@ program vect_mat_mul
     print*, 'Number of columns per process:', Ncolumns
 
     ! Setting DISPLS and RECVCOUNTS for MPI_GATHER
-    allocate(DISPLS(size))
-    allocate(RECVCOUNTS(size))
+    allocate(DISPLS(size)) !width of resulting vector for each process
+    allocate(RECVCOUNTS(size)) ! index of the first element of the local vector in the global one
 
     disp = 0
     do i=0, size-1
@@ -48,44 +50,33 @@ program vect_mat_mul
         disp = disp + RECVCOUNTS(i+1)
     end do
 
-    print*, 'DISPLS:', DISPLS
-    print*, 'RECVCOUNTS:', RECVCOUNTS
-    !if 
-
     ! If the number of columns is not divisible by size increase by one
-    ! the first elements
+    ! the first x elements, with x being the reminder of n/size
    
     if (rank < remainder) then
         Ncolumns = Ncolumns + 1
     end if
 
-    print*, 'Adjusted number of columns for process', rank, ':', Ncolumns
-
-    ! Initialize Vector
+    ! Initialize Vector as [ 1,2,...,n ]
     allocate(V(n))   
     
     do i=1,n
         V(i) = i
     end do
 
-    ! Initialize Matrix
+    ! Initialize Matrix with n rows, each one like [ 1,2,...,n ]
     allocate(M(n,Ncolumns))
 
     do i=1,n
         do j = 1, Ncolumns
-        !do j=DISPLS(rank)+1, DISPLS(rank)+1 + RECVCOUNTS(rank)
             M(i, j) = DISPLS(rank+1) + j
         end do
     end do
-    
-    ! do i=1,n
-    !     print*, M(i,:)
-    ! end do
 
     ! initialize resulting vector
     allocate(C(Ncolumns))
 
-    !C = MATMUL(V, M)
+    !Computing the vector-matrix product
     do j =1, Ncolumns
         C(j) = 0.0
         do k=1, n
@@ -93,24 +84,27 @@ program vect_mat_mul
         end do
     end do
 
-    !communication part
-    
-    !call MPI_GATHERV(C, Ncolumns, MPI_DOUBLE_PRECISION, buffer, RECVCOUNTS, DISPLS, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
-    if (rank == 0) then
-        n_print = MIN(n, 100)
+    ! COMMUNICATION AND SAVING DATA SECTION
 
+    ! saving the fisrts 100 elemnts to the input file
+    ! rank 0 receive all resulting C vectors from others ranks
+    if (rank == 0) then
+        n_print = MIN(n, 100) ! number of elements to be printed
+        
+        ! saving the fisrts 100 elemnts to the input file
         OPEN(12,FILE="ProdVectMat-Receive-Cycle_f90.output",STATUS="REPLACE")
         do i=1, min(n_print, Ncolumns)
             WRITE(12,*) C(i)
             n_print = n_print -1
         end do
         
-        !iterating over the tasks
+        !iterating over the tasks to receive receive
         do i=1, size-1
             n_elements = RECVCOUNTS(i+1)
-            allocate(buffer(n_elements))
+            allocate(buffer(n_elements))! initialize receive buffer
+            ! receiving the C i-th vector into buffer
             call MPI_RECV(buffer, n_elements, MPI_DOUBLE_PRECISION, i, 10, MPI_COMM_WORLD, status, ierr)
-            !print*, buffer()
+            !writing the elements if still needed
             if (n_print > 0) then
                 do j=1, min(n_print, n_elements)
                     WRITE(12,*) buffer(j)
@@ -121,20 +115,23 @@ program vect_mat_mul
         end do
         CLOSE(12, STATUS='KEEP')
     else
+        ! for tasks 1,...,n sending the C vector to rank 0
         call MPI_SEND(C, Ncolumns, MPI_DOUBLE_PRECISION, 0, 10, MPI_COMM_WORLD, ierr)
     end if
 
+    ! stop time measurment
     CALL CPU_TIME(cpu2)
 
+    ! saving computing time into correspondent times output file 
     deltat = cpu2 - cpu1
     if (rank == 0) then
         WRITE(times_filename, '(A,I0,A)') 'times_', n, '_ProdVectMat-Receive-Cycle_f90.txt'
         OPEN(13,FILE=times_filename,status='unknown', position='append')
         WRITE(13, '(I2, X, F10.6)') size, deltat
         CLOSE(13, STATUS='KEEP')
-        !print*, 'Time taken for initialization:', deltat
     end if
 
+    ! finalizing parallel part of the code
     CALL MPI_FINALIZE(ierr)
 end program vect_mat_mul
 
